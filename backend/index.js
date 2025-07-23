e37e1d4e36220478ba279ba0defa6aa9b3122724
“vivek3tech” <vivek3tech@gmail.com>
2025-07-23 23:51:11 +0530

    commit Dockerfile and chnaged port

--- backend/index.js
+++ backend/index.js
@@ -82,7 +82,7 @@ app.post('/send-money', async (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 3001;
-app.listen(PORT, () => {
-  console.log(`Backend API listening on port ${PORT}`);
-}); 
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server listening on port ${PORT}`);
+});
